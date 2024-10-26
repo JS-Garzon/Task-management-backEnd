22909f67bb7e133edba8f88f134ede14668053f8
sebastian garzon <juansebastiangarzonchamorro@gmail.com>
2024-10-25 22:12:45 -0500

    New url was added fro CORS

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,10 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: [
+      'http://localhost:4200',
+      'https://task-management-backend-production-d570.up.railway.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
